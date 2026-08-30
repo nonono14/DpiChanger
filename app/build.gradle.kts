c32c5186fd6f78cb65eb1e7d79b98d4d16217e25
nonono14 <reshetkovzhenya@gmail.com>
2026-08-30 09:33:42 +0300

    Create build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,54 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.zhenya.dpichanger"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.zhenya.dpichanger"
+        minSdk = 30
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
+    buildFeatures {
+        compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.14"
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.activity:activity-compose:1.9.0")
+
+    implementation("androidx.wear.compose:compose-material3:1.4.0")
+    implementation("androidx.wear.compose:compose-foundation:1.4.0")
+    implementation("androidx.wear.compose:compose-navigation:1.4.0")
+
+    implementation("androidx.compose.ui:ui:1.6.8")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.6.8")
+    implementation("androidx.wear:wear-tooling-preview:1.0.0")
+    implementation("com.google.android.gms:play-services-wearable:18.2.0")
+}
