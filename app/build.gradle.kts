62347a2cb924cb480fb2d4f64c7aa2924a2e9442
nonono14 <reshetkovzhenya@gmail.com>
2026-08-30 10:05:47 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.zhenya.dpichanger"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.zhenya.dpichanger"
         minSdk = 30
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
     }
